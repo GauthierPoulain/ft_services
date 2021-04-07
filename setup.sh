0d6b8021a15260aae32c1d5a04cc69ccea3073a9
gauthier poulain <gapoulai@student.42lyon.fr>
2021-04-08 01:53:03 +0200

    setup nginx

--- setup.sh
+++ setup.sh
@@ -1,57 +1,47 @@
 #!/bin/bash
 clear
+minikube start --extra-config=apiserver.service-node-port-range=1-65535
 
-print_usage() {
-  echo "usage :"
-  echo "-r : reset minikube"
+metallb(){
+	kubectl apply -f srcs/metallb/namespace.yaml
+	kubectl apply -f srcs/metallb/metallb.yaml
+
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+	kubectl apply -f srcs/metallb/config.yaml
 }
 
-reset_minikube() {
-	echo "minikube is cleaning up..."
-	minikube stop
-	minikube delete
-	echo "minikube is clean"
+wordpress(){
+	echo "downloading wordpress..."
+	WP_DOWNLOADING_PATH=./srcs/wordpress/tmp
+	mkdir -p $WP_DOWNLOADING_PATH
+	if ! [ -f $WP_DOWNLOADING_PATH/wordpress.tar.gz ]; then
+		curl -Lo $WP_DOWNLOADING_PATH/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+	else 
+		echo "wordpress already exist ($WP_DOWNLOADING_PATH/wordpress.tar.gz)"
+	fi
+
+
+	docker build srcs/wordpress --rm -t ft-services-wordpress
+	kubectl apply -f srcs/wordpress/wordpress.yaml
 }
 
-while getopts 'r' flag; do
-  case "${flag}" in
-    r) reset_minikube ;;
-    *) print_usage
-       exit 1 ;;
-  esac
-done
+nginx(){
+	docker build srcs/nginx --rm -t ft-services-nginx
+	kubectl apply -f srcs/nginx/nginx.yaml
+}
 
-minikube start
+run(){
+	kubectl delete deployments --all
+	kubectl delete services wordpress-service
+	kubectl delete services nginx-service
+	metallb
+	nginx
+	wordpress
+	echo "ready to serve on $(minikube ip)"
+}
+
 eval $(minikube docker-env)
 
-# metallb -------------------
-
-kubectl apply -f srcs/metallb/namespace.yaml
-kubectl apply -f srcs/metallb/metallb.yaml
-
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
-
-# /metallb ------------------
-
-# wordpress -----------------
-
-echo "downloading wordpress..."
-WP_DOWNLOADING_PATH=./srcs/wordpress/tmp
-mkdir -p $WP_DOWNLOADING_PATH
-if ! [ -f $WP_DOWNLOADING_PATH/wordpress.tar.gz ]; then
-	curl -Lo $WP_DOWNLOADING_PATH/wordpress.tar.gz https://wordpress.org/latest.tar.gz
-else 
-	echo "wordpress already exist ($WP_DOWNLOADING_PATH/wordpress.tar.gz)"
-fi
-
-
-docker build srcs/wordpress --rm -t ft-services-wordpress
-echo "pushing wordpress image to minikube..."
-kubectl apply -f srcs/wordpress/wordpress.yaml
-
-# /wordpress ----------------
-
-kubectl apply -f srcs/metallb/config.yaml
+run &
 
 minikube dashboard
---
+++ srcs/nginx/init.sh
@@ -0,0 +1,2 @@
+rc-service nginx start
+tail -f /var/log/nginx/access.log -f /var/log/nginx/error.log
