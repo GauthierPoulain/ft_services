c567f07dfb935d127f490d3d1829f5590bd04f67
gauthier poulain <gapoulai@student.42lyon.fr>
2021-04-08 14:59:59 +0200

    fix quota limit

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 clear
 minikube start --extra-config=apiserver.service-node-port-range=1-65535
 
-metallb(){
+eval $(minikube docker-env)
+minikube ssh "docker login -u gapoulai -p motdepassesupersafe"
+
+metallb() {
 	kubectl apply -f srcs/metallb/namespace.yaml
 	kubectl apply -f srcs/metallb/metallb.yaml
 
@@ -10,38 +13,43 @@ metallb(){
 	kubectl apply -f srcs/metallb/config.yaml
 }
 
-wordpress(){
+wordpress() {
 	echo "downloading wordpress..."
 	WP_DOWNLOADING_PATH=./srcs/wordpress/tmp
 	mkdir -p $WP_DOWNLOADING_PATH
 	if ! [ -f $WP_DOWNLOADING_PATH/wordpress.tar.gz ]; then
 		curl -Lo $WP_DOWNLOADING_PATH/wordpress.tar.gz https://wordpress.org/latest.tar.gz
-	else 
+	else
 		echo "wordpress already exist ($WP_DOWNLOADING_PATH/wordpress.tar.gz)"
 	fi
-
 
 	docker build srcs/wordpress --rm -t ft-services-wordpress
 	kubectl apply -f srcs/wordpress/wordpress.yaml
 }
 
-nginx(){
+nginx() {
 	docker build srcs/nginx --rm -t ft-services-nginx
 	kubectl apply -f srcs/nginx/nginx.yaml
 }
 
-run(){
-	kubectl delete deployments --all
-	kubectl delete services wordpress-service
-	kubectl delete services nginx-service
+pull_images() {
+	minikube ssh "docker pull metallb/controller:v0.9.6"
+	minikube ssh "docker pull metallb/speaker:v0.9.6"
+	minikube ssh "docker pull alpine:latest"
+}
+
+run() {
+	# kubectl delete deployments --all
+	# kubectl delete pods --all
+	# kubectl delete services wordpress-service
+	# kubectl delete services nginx-service
+	pull_images
 	metallb
 	nginx
 	wordpress
 	echo "ready to serve on $(minikube ip)"
 }
 
-eval $(minikube docker-env)
-
-run &
+run
 
 minikube dashboard
