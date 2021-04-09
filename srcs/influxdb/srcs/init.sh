d5345c96e09922492c1e90696a840b66cdf8b4cb
gauthier poulain <gapoulai@student.42lyon.fr>
2021-04-09 21:04:20 +0200

    some tweaks

---
+++ srcs/influxdb/srcs/init.sh
@@ -0,0 +1,17 @@
+check_service() {
+	for var in "$@"; do
+		if [ $(/usr/bin/pgrep $var | wc -l) == 0 ]; then
+			exit 1
+		fi
+	done
+}
+
+rc-service influxdb start
+telegraf &
+
+sleep 5
+
+while true; do
+	check_service influx telegraf
+	sleep 2
+done
--- srcs/restart.sh
+++ srcs/restart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-kubectl delete -f $1
-kubectl apply -f $1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $DIR
+eval $(minikube docker-env)
+kubectl delete -f $1/$1.yaml
+docker build ./$1 --rm -t ft-services-$1
+kubectl apply -f $1/$1.yaml
